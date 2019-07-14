86f112e43c05af7ec80088196d16502d01699d7c
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-14 06:01:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,14 +4,15 @@
 def run_guessing_game
   num = rand(6)+1
   guess = gets
-  #while true do
+  while true do
     guess = user_guess
     if guess == 'exit'
-      #break
+      break
       elsif guess == num
       puts "You guessed the correct number!"
     else
       puts "Sorry! The computer guessed #{num}"
     end
+  end
   puts "Goodbye!"
 end
